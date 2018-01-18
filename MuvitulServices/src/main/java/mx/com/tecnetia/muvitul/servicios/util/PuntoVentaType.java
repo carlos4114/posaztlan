87f551b49d543e6df75cbfbf855f3ef67b8eb35a
Carlos <Carlos@Carlos-PC>
2018-01-18 17:21:46 -0600

    tipo de punto de venta 3

--- MuvitulServices/src/main/java/mx/com/tecnetia/muvitul/servicios/util/PuntoVentaType.java
+++ MuvitulServices/src/main/java/mx/com/tecnetia/muvitul/servicios/util/PuntoVentaType.java
@@ -1,7 +1,7 @@
 package mx.com.tecnetia.muvitul.servicios.util;
 
 public enum PuntoVentaType {
-	TAQUILLA("TAQ-002"), DULCERIA("DUL-001");
+	TAQUILLA("TAQ-002"), DULCERIA("DUL-001"), TAQUILLA_DULCERIA("TAQDUL-003");
 	private String type;
 
 	public String getType() {
